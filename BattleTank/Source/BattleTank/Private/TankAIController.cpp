7431ae7e919597bab0cad731e601842d1d95404b
ANCHOR\Dave <Dave@RevThornton.com>
2016-11-24 20:52:43 -0800

    Video 118  help  skynet find its  target the humans!!!

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,34 @@
+// Copyright David Thornton 2016
+
+#include "BattleTank.h"
+#include "tank.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	auto ControlledTank = GetControlledTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller %s  cant find a Player Pawn"), *(ControlledTank->GetName()), *(PlayerTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller %s Found Player Pawn %s"), *(ControlledTank->GetName()), *(PlayerTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	auto ControlledTank = GetControlledTank();
+	if (!PlayerPawn){return nullptr;}
+	return Cast<ATank>(PlayerPawn);
+}
+
---
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -0,0 +1,23 @@
+// Copyright David Thornton 2016
+
+#pragma once
+
+#include "AIController.h"
+#include "Tank.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;
+
+	virtual void BeginPlay() override;
+	
+	ATank* GetPlayerTank() const;
+};
