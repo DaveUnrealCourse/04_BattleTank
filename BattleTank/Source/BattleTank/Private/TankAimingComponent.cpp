6403dba08644ef834b0795d46ada5111d6d1c677
ANCHOR\Dave <Dave@RevThornton.com>
2016-12-03 14:05:34 -0800

    Video  133

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -10,7 +10,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true;//TODO should this really tick
 	// ...
 }
 void UTankAimingComponent::SetBarrelReference(UTankBarrel* BarrelToSet)
@@ -34,7 +34,14 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();	
 		MoveBarrelTowards(AimDirection);
-	};
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f : Aim Soulation is found"), Time);
+	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f : There is No Aiming Soulation"), Time);
+	}
 }
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
