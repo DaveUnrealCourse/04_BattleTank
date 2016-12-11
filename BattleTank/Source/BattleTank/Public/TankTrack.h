521c64d695afdadf1470d0d52e4209e07f50d371
ANCHOR\Dave <Dave@RevThornton.com>
2016-12-10 18:40:12 -0800

    working Now?

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -16,7 +16,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	
 void SetThrottle(float Throttle);
-	//Max Force in Newtons
+	//Max Force in Newtons!!!
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 		float TrackMaxDrivingFource = 40000000;//Assume 40 ton tank and 1 g of exceleration
 	
